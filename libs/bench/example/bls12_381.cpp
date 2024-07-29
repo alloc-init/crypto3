17362f7e6ea5d0eaf24783b506becec86b8e9c16
Vasiliy Olekhov <vasiliy.olekhov@nil.foundation>
2024-07-29 09:44:10 +0300

    wip [ci skip]

--- libs/bench/example/bls12_381.cpp
+++ libs/bench/example/bls12_381.cpp
@@ -9,6 +9,7 @@
 
 #include <nil/crypto3/algebra/curves/bls12.hpp>
 #include <nil/crypto3/algebra/curves/mnt4.hpp>
+#include <nil/crypto3/algebra/curves/pallas.hpp>
 
 #include <boost/random.hpp>
 #include <boost/random/random_device.hpp>
@@ -17,6 +18,33 @@
 
 int main()
 {
+    {
+        using bench_type_A = bench_type<nil::crypto3::algebra::curves::mnt4_298::base_field_type>;
+        using bench_type_B = bench_type_A;
+        using bench_type_C = bench_type_A;
+
+        CRYPTO3_RUN_BENCHMARK("MNT4-298 base field multiplication", "sample.json",
+                bench_type_A, bench_type_B, bench_type_C,
+                A = B * C
+                );
+    }
+    {
+        using bench_type_A = bench_type<nil::crypto3::algebra::curves::mnt4_298::base_field_type>;
+        using bench_type_B = bench_type_A;
+        using bench_type_C = bench_type_A;
+        auto op = [](
+                bench_type_A::type_name::value_type & la,
+                bench_type_B::type_name::value_type const& lb,
+                bench_type_C::type_name::value_type const& lc) { 
+            la = lb * lc;
+        };
+
+        CRYPTO3_RUN_BENCHMARK("MNT4-298 base field multiplication, lambda", "sample.json",
+                bench_type_A, bench_type_B, bench_type_C,
+
+                op(A,B,C);
+                );
+    }
     /*
     {
         using bench_type_A = bench_type<nil::crypto3::algebra::curves::mnt4_298::g1_type<>>;
@@ -65,6 +93,7 @@ int main()
                 );
     }
     */
+    /*
     {
         using bench_type_A = bench_type<nil::crypto3::algebra::curves::bls12_381::base_field_type>;
         using bench_type_B = bench_type_A;
@@ -75,6 +104,8 @@ int main()
                 A = B * C
                 );
     }
+    */
+    /*
     {
         using bench_type_A = bench_type<nil::crypto3::algebra::curves::bls12_381::scalar_field_type>;
         using bench_type_B = bench_type_A;
@@ -85,5 +116,18 @@ int main()
                 A = B * C
                 );
     }
+    */
+    /*
+    {
+        using bench_type_A = bench_type<nil::crypto3::algebra::curves::pallas::base_field_type>;
+        using bench_type_B = bench_type_A;
+        using bench_type_C = bench_type_A;
+
+        CRYPTO3_RUN_BENCHMARK("Pallas base field multiplication", "sample.json",
+                bench_type_A, bench_type_B, bench_type_C,
+                A = B * C
+                );
+    }
+    */
     return 0;
 }
---
+++ libs/bench/include/nil/crypto3/bench/runner.hpp
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2024 Vasiliy Olekhov <vasiliy.olekhov@nil.foundation>
+//
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+#include <nil/crypto3/algebra/random_element.hpp>
+
+namespace nil {
+    namespace crypto3 {
+        namespace benchmark {
+
+
+template<typename A, typename B, typename C>
+struct {};
+
+
+#define CRYPTO3_BENCHMARK(name, func) \
+    namespace { static ::nil::crypto3::benchmark::registry 
+
+
+        }
+    }
+}
+
