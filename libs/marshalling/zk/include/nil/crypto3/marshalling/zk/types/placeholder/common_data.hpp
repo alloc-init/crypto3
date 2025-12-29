5023f96ac59295c3f671c4439f2bc27480cf9a1f
Brent Carmer <bcarmer@gmail.com>
2025-12-29 10:19:50 -0800

    fix

--- libs/marshalling/zk/include/nil/crypto3/marshalling/zk/types/placeholder/common_data.hpp
+++ libs/marshalling/zk/include/nil/crypto3/marshalling/zk/types/placeholder/common_data.hpp
@@ -147,12 +147,9 @@ namespace nil {
                     if constexpr(nil::crypto3::algebra::is_field_element<
                         typename CommonDataType::transcript_hash_type::word_type
                     >::value) {
-                        auto integral =
-                            typename CommonDataType::field_type::integral_type(
-                                common_data.vk.constraint_system_with_params_hash
-                                    .to_integral());
+                        auto integral = typename CommonDataType::field_type::integral_type(common_data.vk.constraint_system_with_params_hash.data);
                         std::vector<unsigned char> blob;
-                        integral.export_bits(std::back_inserter(blob), 8);
+                        export_bits(integral, std::back_inserter(blob), 8);
                         for( std::size_t i = blob.size(); i > 0; i--){
                             filled_constraint_system_with_params_hash.value().push_back(
                                 nil::marshalling::types::integral<TTypeBase, octet_type>(blob[i-1])
@@ -250,7 +247,7 @@ namespace nil {
                             blob.push_back(std::uint8_t(std::get<13>(filled_common_data.value()).value()[i].value()));
                         }
                         typename CommonDataType::field_type::integral_type newval;
-                        newval.import_bits(blob.begin(), blob.end(), 8, false);
+                        import_bits(newval, blob.begin(), blob.end(), 8, false);
                         vk.constraint_system_with_params_hash = typename CommonDataType::field_type::value_type(newval);
                     } else {
                         for( std::size_t i = 0; i < std::get<13>(filled_common_data.value()).value().size(); i++){
