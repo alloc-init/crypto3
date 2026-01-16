88fb3f44a7ff9a8dcd3269b0b4924d6b25267146
Brent Carmer <bcarmer@gmail.com>
2026-01-16 12:40:29 -0800

    script fix

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -73,6 +73,7 @@ shift $((OPTIND-1))
 function list_make_targets() {
     make -qp \
         | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' \
+        | grep _test \
         | sort -u
 }
 
