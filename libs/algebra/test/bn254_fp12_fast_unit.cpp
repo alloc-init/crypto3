9a6b958d0a74af97c607abdee49ad113c54dbeb4
Brent Carmer <bcarmer@gmail.com>
2026-06-03 14:38:41 -0700

    update unit

--- libs/algebra/test/bn254_fp12_fast_unit.cpp
+++ libs/algebra/test/bn254_fp12_fast_unit.cpp
@@ -31,14 +31,48 @@
 #include <boost/random/uniform_int_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
 
-#include <nil/crypto3/algebra/fields/bn128/base_field.hpp>
-#include <nil/crypto3/algebra/curves/alt_bn128.hpp>
+#include <nil/crypto3/algebra/fields/alt_bn128/base_field.hpp>
+#include <nil/crypto3/algebra/fields/detail/extension_params/alt_bn128/detail/fp12_limb_ops.hpp>
 
 using namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops;
 
 BOOST_AUTO_TEST_SUITE(bn254_fp12_fast_unit_tests)
 
 #if defined(__x86_64__) && (defined(__GNUC__) || defined(__clang__))
+namespace {
+    using field_type = nil::crypto3::algebra::fields::alt_bn128<254>;
+
+    limb_array random_limbs(boost::random::mt19937 &rng,
+                            boost::random::uniform_int_distribution<limb> &distribution,
+                            size_t count) {
+        limb_array result = {};
+        for (size_t i = 0; i < count; i++) {
+            result[i] = distribution(rng);
+        }
+        return result;
+    }
+
+    void require_reduce_x86_matches_portable(const limb_array &input) {
+        limb_array expected = input;
+        limb_array actual = input;
+        montgomery_reduce_portable<field_type>(expected);
+        montgomery_reduce_x86<field_type>(actual);
+        BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), actual.begin(), actual.end());
+    }
+
+    void require_reduce_product_4x4_matches(const limb_array &x, const limb_array &y) {
+        limb_array product = {};
+        multiply_4x4_portable(product, x, y);
+        require_reduce_x86_matches_portable(product);
+    }
+
+    void require_reduce_product_5x5_matches(const limb_array &x, const limb_array &y) {
+        limb_array product = {};
+        multiply_5x5_portable(product, x, y);
+        require_reduce_x86_matches_portable(product);
+    }
+}    // namespace
+
 BOOST_AUTO_TEST_CASE(mul_4x4_x86_random) {
     boost::random::mt19937 rng(0x2545);
     boost::random::uniform_int_distribution<limb> d;
@@ -67,26 +101,56 @@ BOOST_AUTO_TEST_CASE(mul_5x5_x86_random) {
             x[i] = d(rng);
             y[i] = d(rng);
         }
+        x[4] &= 1u;
+        y[4] &= 1u;
         multiply_5x5_portable(z0, x, y);
         multiply_5x5_x86(z1, x, y);
         BOOST_REQUIRE_EQUAL_COLLECTIONS(z0.begin(), z0.end(), z1.begin(), z1.end());
     }
 }
 
-BOOST_AUTO_TEST_CASE(reduce_x86_random) {
-    using field_type = nil::crypto3::algebra::fields::alt_bn128<254>;
+BOOST_AUTO_TEST_CASE(reduce_x86_random_products) {
     boost::random::mt19937 rng(0x2545);
     boost::random::uniform_int_distribution<limb> d;
     for (size_t i = 0; i < 100; i++) {
-        limb_array x0;
-        for (size_t i = 0; i < 9; i++) {
-            x0[i] = d(rng);
-        }
-        limb_array x1 = x0;
-        montgomery_reduce_portable<field_type>(x0);
-        montgomery_reduce_x86<field_type>(x1);
-        BOOST_REQUIRE_EQUAL_COLLECTIONS(x0.begin(), x0.end(), x1.begin(), x1.end());
+        const limb_array x = random_limbs(rng, d, 4);
+        const limb_array y = random_limbs(rng, d, 4);
+        require_reduce_product_4x4_matches(x, y);
     }
+    for (size_t i = 0; i < 100; i++) {
+        limb_array x = random_limbs(rng, d, 5);
+        limb_array y = random_limbs(rng, d, 5);
+        x[4] &= 1u;
+        y[4] &= 1u;
+        require_reduce_product_5x5_matches(x, y);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(reduce_x86_edge_products) {
+    const limb max = ~limb(0u);
+
+    limb_array all_max_4 = {};
+    for (size_t i = 0; i < 4; i++) {
+        all_max_4[i] = max;
+    }
+
+    limb_array alternating_a = {};
+    limb_array alternating_b = {};
+    for (size_t i = 0; i < 4; i++) {
+        alternating_a[i] = (i % 2 == 0) ? max : 0u;
+        alternating_b[i] = (i % 2 == 0) ? 0u : max;
+    }
+
+    limb_array high_5_a = all_max_4;
+    limb_array high_5_b = all_max_4;
+    high_5_a[4] = 1u;
+    high_5_b[4] = 1u;
+
+    require_reduce_product_4x4_matches(all_max_4, all_max_4);
+    require_reduce_product_4x4_matches(all_max_4, alternating_a);
+    require_reduce_product_4x4_matches(alternating_a, alternating_b);
+    require_reduce_product_5x5_matches(high_5_a, high_5_b);
+    require_reduce_product_5x5_matches(high_5_a, all_max_4);
 }
 #endif
 
