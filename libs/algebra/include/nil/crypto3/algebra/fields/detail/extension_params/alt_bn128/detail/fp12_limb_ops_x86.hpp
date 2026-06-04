e306805d35de7b926d7f0ff8aa4ca31fce93ef3e
Brent Carmer <bcarmer@gmail.com>
2026-06-04 07:11:20 -0700

    tidy

--- libs/algebra/include/nil/crypto3/algebra/fields/detail/extension_params/alt_bn128/detail/fp12_limb_ops_x86.hpp
+++ libs/algebra/include/nil/crypto3/algebra/fields/detail/extension_params/alt_bn128/detail/fp12_limb_ops_x86.hpp
@@ -6,20 +6,20 @@
 #define STR_IMPL(X) #X
 #define STR(X) STR_IMPL(X)
 
-#define BYTE_OFFSET(REGNAME, I) STR(BOOST_PP_MUL(I, 8)) "(%[" #REGNAME "])"
-#define BYTE_OFFSET2(REGNAME, I, J) BYTE_OFFSET(REGNAME, BOOST_PP_ADD(I, J))
+#define PTR(REGNAME, I) STR(BOOST_PP_MUL(I, 8)) "(%[" #REGNAME "])"
+#define PTR2(REGNAME, I, J) PTR(REGNAME, BOOST_PP_ADD(I, J))
 
-#define bn254_fp12_multiply_partial_x86(X, Y)   \
-    "movq " BYTE_OFFSET(x, X) ", %%rax\n"         \
-    "mulq " BYTE_OFFSET(y, Y) "\n"              \
-    "add %%rax, %[acc0]\n"                      \
-    "adc %%rdx, %[acc1]\n"                      \
+#define bn254_fp12_multiply_partial_x86(X, Y) \
+    "movq " PTR(x, X) ", %%rax\n"             \
+    "mulq " PTR(y, Y) "\n"                    \
+    "add %%rax, %[acc0]\n"                    \
+    "adc %%rdx, %[acc1]\n"                    \
     "adc $0, %[acc2]\n"
 
-#define bn254_fp12_multiply_emit_x86(I)             \
-    "movq %[acc0], " BYTE_OFFSET(result, I) "\n"    \
-    "movq %[acc1], %[acc0]\n"                       \
-    "movq %[acc2], %[acc1]\n"                       \
+#define bn254_fp12_multiply_emit_x86(I)  \
+    "movq %[acc0], " PTR(result, I) "\n" \
+    "movq %[acc1], %[acc0]\n"            \
+    "movq %[acc2], %[acc1]\n"            \
     "xor %[acc2], %[acc2]\n"
 
 namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
@@ -45,30 +45,29 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
             bn254_fp12_multiply_partial_x86(2,0)
             bn254_fp12_multiply_emit_x86(2)
 
-                // round 4, x0*y3, x1*y2, x2*y1, x3*y0
+            // round 4, x0*y3, x1*y2, x2*y1, x3*y0
             bn254_fp12_multiply_partial_x86(0, 3)
             bn254_fp12_multiply_partial_x86(1, 2)
             bn254_fp12_multiply_partial_x86(2, 1)
             bn254_fp12_multiply_partial_x86(3, 0)
             bn254_fp12_multiply_emit_x86(3)
 
-                // round 5, x1*y3, x2*y2, x3*y1
+            // round 5, x1*y3, x2*y2, x3*y1
             bn254_fp12_multiply_partial_x86(1, 3)
             bn254_fp12_multiply_partial_x86(2, 2)
             bn254_fp12_multiply_partial_x86(3, 1)
             bn254_fp12_multiply_emit_x86(4)
 
-                // round 6, x2*y3, x3*y2
+            // round 6, x2*y3, x3*y2
             bn254_fp12_multiply_partial_x86(2, 3)
             bn254_fp12_multiply_partial_x86(3, 2)
             bn254_fp12_multiply_emit_x86(5)
 
-                // round 7, x3*y3
+            // round 7, x3*y3
             bn254_fp12_multiply_partial_x86(3, 3)
             bn254_fp12_multiply_emit_x86(6)
-            "movq %[acc0], " BYTE_OFFSET(result, 7) "\n"
-            "movq %[acc1], " BYTE_OFFSET(result, 8) "\n"
-
+            "movq %[acc0], " PTR(result, 7) "\n"
+            "movq %[acc1], " PTR(result, 8) "\n"
 
             :   [acc0]"+r"(acc0),
                 [acc1]"+r"(acc1),
@@ -101,14 +100,14 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
             bn254_fp12_multiply_partial_x86(2, 0)
             bn254_fp12_multiply_emit_x86(2)
 
-                // round 4, x0*y3, x1*y2, x2*y1, x3*y0
+            // round 4, x0*y3, x1*y2, x2*y1, x3*y0
             bn254_fp12_multiply_partial_x86(0, 3)
             bn254_fp12_multiply_partial_x86(1, 2)
             bn254_fp12_multiply_partial_x86(2, 1)
             bn254_fp12_multiply_partial_x86(3, 0)
             bn254_fp12_multiply_emit_x86(3)
 
-                // round 5, x0*y4, x1*y3, x2*y2, x3*y1, x4*y0
+            // round 5, x0*y4, x1*y3, x2*y2, x3*y1, x4*y0
             bn254_fp12_multiply_partial_x86(0, 4)
             bn254_fp12_multiply_partial_x86(1, 3)
             bn254_fp12_multiply_partial_x86(2, 2)
@@ -116,29 +115,29 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
             bn254_fp12_multiply_partial_x86(4, 0)
             bn254_fp12_multiply_emit_x86(4)
 
-                // round 6, x1*y4, x2*y3, x3*y2, x4*y1
+            // round 6, x1*y4, x2*y3, x3*y2, x4*y1
             bn254_fp12_multiply_partial_x86(1, 4)
             bn254_fp12_multiply_partial_x86(2, 3)
             bn254_fp12_multiply_partial_x86(3, 2)
             bn254_fp12_multiply_partial_x86(4, 1)
             bn254_fp12_multiply_emit_x86(5)
 
-                // round 7, x2*y4, x3*y3, x4*y2
+            // round 7, x2*y4, x3*y3, x4*y2
             bn254_fp12_multiply_partial_x86(2, 4)
             bn254_fp12_multiply_partial_x86(3, 3)
             bn254_fp12_multiply_partial_x86(4, 2)
             bn254_fp12_multiply_emit_x86(6)
 
-                // round 8, x3*y4, x4*y3
+            // round 8, x3*y4, x4*y3
             bn254_fp12_multiply_partial_x86(3, 4)
             bn254_fp12_multiply_partial_x86(4, 3)
             bn254_fp12_multiply_emit_x86(7)
 
-                // round 9, x4*y4, drop high bits (see note in portable version)
-            "movq " BYTE_OFFSET(x, 4) ", %%rax\n"
-            "mulq " BYTE_OFFSET(y, 4) "\n"
+            // round 9, x4*y4, drop high bits (see note in portable version)
+            "movq " PTR(x, 4) ", %%rax\n"
+            "mulq " PTR(y, 4) "\n"
             "add %%rax, %[acc0]\n"
-            "movq %[acc0], " BYTE_OFFSET(result, 8) "\n"
+            "movq %[acc0], " PTR(result, 8) "\n"
 
             :   [acc0]"+r"(acc0),
                 [acc1]"+r"(acc1),
@@ -152,14 +151,14 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
 }    // namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops
 
 // get the i+j%5-th "t" register
-#define REG_T_IJ_MOD5(I, J) "%[t" STR(BOOST_PP_MOD(BOOST_PP_ADD(I, J), 5)) "]"
+#define T(I, J) "%[t" STR(BOOST_PP_MOD(BOOST_PP_ADD(I, J), 5)) "]"
 
 // multiply bottom limb by m*p and propagate carries
 #define bn254_fp12_montgomery_reduce_mul_mp(I, J)               \
     /* compute m * p[j], m is in rdx */                         \
     "mulxq %[p" #J "], %[low], %[high] \n"                      \
     /* carry += data[i+j] // add data to carry accumulator */   \
-    "add " REG_T_IJ_MOD5(I, J) ", %[carry]\n"                        \
+    "add " T(I, J) ", %[carry]\n"                               \
     /* add any overflow to high */                              \
     "adc $0, %[high]\n"                                         \
     /* add carry/accumulator to low */                          \
@@ -167,7 +166,7 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
     /* add overflow to high */                                  \
     "adc $0, %[high]\n"                                         \
     /* data[i,j] = low */                                       \
-    "movq %[low], " REG_T_IJ_MOD5(I, J) "\n"                         \
+    "movq %[low], " T(I, J) "\n"                                \
     /* carry = high */                                          \
     "movq %[high], %[carry]\n"
 
@@ -185,9 +184,9 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
     bn254_fp12_montgomery_reduce_mul_mp(I, 2)                   \
     bn254_fp12_montgomery_reduce_mul_mp(I, 3)                   \
     /* load next limb */                                        \
-    "movq " BYTE_OFFSET2(data, I, 5) ", %[t" #I "]\n"           \
+    "movq " PTR2(data, I, 5) ", %[t" #I "]\n"                   \
     /* propagate carries */                                     \
-    "add %[carry], " REG_T_IJ_MOD5(I, 4) "\n"                   \
+    "add %[carry], " T(I, 4) "\n"                               \
     "adcq %[pending], %[t" #I "]\n"                             \
     "setc %b[pending]\n"
 
@@ -204,11 +203,11 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
         limb t0, t1, t2, t3, t4, low, high, pending, carry;
 
         asm volatile(
-            "movq " BYTE_OFFSET(data, 0) ", %[t0]\n"
-            "movq " BYTE_OFFSET(data, 1) ", %[t1]\n"
-            "movq " BYTE_OFFSET(data, 2) ", %[t2]\n"
-            "movq " BYTE_OFFSET(data, 3) ", %[t3]\n"
-            "movq " BYTE_OFFSET(data, 4) ", %[t4]\n"
+            "movq " PTR(data, 0) ", %[t0]\n"
+            "movq " PTR(data, 1) ", %[t1]\n"
+            "movq " PTR(data, 2) ", %[t2]\n"
+            "movq " PTR(data, 3) ", %[t3]\n"
+            "movq " PTR(data, 4) ", %[t4]\n"
 
             // initial loop: for each limb, compute m and multiply each limb by m*p
             // make sure window carry is initialized
@@ -220,37 +219,37 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
 
             // subtract modulus
             "modulus_loop_start%=:\n"
-            "testq " REG_T_IJ_MOD5(4, 4) ", " REG_T_IJ_MOD5(4, 4) "\n"
+            "testq " T(4, 4) ", " T(4, 4) "\n"
             "jnz modulus_loop_subtract%=\n"
-            "cmpq %[p3], " REG_T_IJ_MOD5(3, 4) "\n"
+            "cmpq %[p3], " T(3, 4) "\n"
             "ja modulus_loop_subtract%=\n"
             "jb modulus_loop_end%=\n"
-            "cmpq %[p2], " REG_T_IJ_MOD5(2, 4) "\n"
+            "cmpq %[p2], " T(2, 4) "\n"
             "ja modulus_loop_subtract%=\n"
             "jb modulus_loop_end%=\n"
-            "cmpq %[p1], " REG_T_IJ_MOD5(1, 4) "\n"
+            "cmpq %[p1], " T(1, 4) "\n"
             "ja modulus_loop_subtract%=\n"
             "jb modulus_loop_end%=\n"
-            "cmpq %[p0], " REG_T_IJ_MOD5(0, 4) "\n"
+            "cmpq %[p0], " T(0, 4) "\n"
             "jb modulus_loop_end%=\n"
             "modulus_loop_subtract%=:\n"
-            "subq %[p0], " REG_T_IJ_MOD5(0, 4) "\n"
-            "sbbq %[p1], " REG_T_IJ_MOD5(1, 4) "\n"
-            "sbbq %[p2], " REG_T_IJ_MOD5(2, 4) "\n"
-            "sbbq %[p3], " REG_T_IJ_MOD5(3, 4) "\n"
-            "sbbq $0, " REG_T_IJ_MOD5(4, 4) "\n"
+            "subq %[p0], " T(0, 4) "\n"
+            "sbbq %[p1], " T(1, 4) "\n"
+            "sbbq %[p2], " T(2, 4) "\n"
+            "sbbq %[p3], " T(3, 4) "\n"
+            "sbbq $0, " T(4, 4) "\n"
             "jmp modulus_loop_start%=\n"
             "modulus_loop_end%=:\n"
 
-            "movq " REG_T_IJ_MOD5(0, 4) ", " BYTE_OFFSET(data, 0) "\n"
-            "movq " REG_T_IJ_MOD5(1, 4) ", " BYTE_OFFSET(data, 1) "\n"
-            "movq " REG_T_IJ_MOD5(2, 4) ", " BYTE_OFFSET(data, 2) "\n"
-            "movq " REG_T_IJ_MOD5(3, 4) ", " BYTE_OFFSET(data, 3) "\n"
-            "movq $0, " BYTE_OFFSET(data, 4) "\n"
-            "movq $0, " BYTE_OFFSET(data, 5) "\n"
-            "movq $0, " BYTE_OFFSET(data, 6) "\n"
-            "movq $0, " BYTE_OFFSET(data, 7) "\n"
-            "movq $0, " BYTE_OFFSET(data, 8) "\n"
+            "movq " T(0, 4) ", " PTR(data, 0) "\n"
+            "movq " T(1, 4) ", " PTR(data, 1) "\n"
+            "movq " T(2, 4) ", " PTR(data, 2) "\n"
+            "movq " T(3, 4) ", " PTR(data, 3) "\n"
+            "movq $0, " PTR(data, 4) "\n"
+            "movq $0, " PTR(data, 5) "\n"
+            "movq $0, " PTR(data, 6) "\n"
+            "movq $0, " PTR(data, 7) "\n"
+            "movq $0, " PTR(data, 8) "\n"
 
             : [t0]"+r"(t0),
               [t1]"+r"(t1),
@@ -275,10 +274,10 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
 
 #undef STR_IMPL
 #undef STR
-#undef BYTE_OFFSET
-#undef BYTE_OFFSET2
+#undef PTR
+#undef PTR2
 #undef bn254_fp12_multiply_partial_x86
 #undef bn254_fp12_multiply_emit_x86
-#undef REG_T_IJ_MOD5
+#undef T
 #undef bn254_fp12_montgomery_reduce_mul_mp
 #undef bn254_fp12_montgomery_reduce_cancel_low
