da31ed9d928eb86ac04dd50c62ad268907045ff7
Brent Carmer <bcarmer@gmail.com>
2026-06-09 06:41:40 -0700

    tidy

--- libs/algebra/include/nil/crypto3/algebra/fields/detail/extension_params/alt_bn128/detail/fp12_limb_ops_x86.hpp
+++ libs/algebra/include/nil/crypto3/algebra/fields/detail/extension_params/alt_bn128/detail/fp12_limb_ops_x86.hpp
@@ -86,7 +86,7 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
 
     inline void multiply_5x5_x86(limb_array &result, const limb_array &x, const limb_array &y) {
         multiply_4x4_x86(result, x, y);
-        
+
         limb d0, d1, d2, d3;
         asm volatile(
             // round 8, x4 and y4 can only be carries
@@ -120,8 +120,7 @@ namespace nil::crypto3::algebra::fields::detail::alt_bn128_fp12_limb_ops {
             "andq " PTR(y, 4) ", %[d0]\n"
             "addq %[d0], " PTR(result, 8) "\n"
 
-            : 
-              [d0]"=&r"(d0),
+            : [d0]"=&r"(d0),
               [d1]"=&r"(d1),
               [d2]"=&r"(d2),
               [d3]"=&r"(d3)
