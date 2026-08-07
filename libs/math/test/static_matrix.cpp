65937bd2d0edef09ebb524c3ecca9ffd3060f27e
Brent Carmer <bcarmer@gmail.com>
2026-08-07 13:46:27 -0700

    format

--- libs/math/test/static_matrix.cpp
+++ libs/math/test/static_matrix.cpp
@@ -41,10 +41,11 @@ using namespace nil::crypto3::math;
 using field = fields::goldilocks;
 using value = field::value_type;
 
+static_assert(make_vector(1, 2, 3) == static_vector {1, 2, 3},
+              "make_vector and uniform initialization deduction guide");
 
-static_assert(make_vector(1, 2, 3) == static_vector {1, 2, 3}, "make_vector and uniform initialization deduction guide");
-
-static_assert(make_vector(1, 2, 3) == static_vector {{1, 2, 3}}, "make_vector and aggregate initialization deduction guide");
+static_assert(make_vector(1, 2, 3) == static_vector {{1, 2, 3}},
+              "make_vector and aggregate initialization deduction guide");
 
 static_assert(elementwise([](value x) { return x.inversed(); }, static_vector {1, 2, 4}) ==
                   static_vector<value, 3> {1, 0x7fffffff80000001_cppui_modular64, 0xbfffffff40000001_cppui_modular64},
@@ -70,7 +71,6 @@ static_assert(static_vector {1, 2, 3} == slice<3>(static_vector {1, 2, 3, 4}), "
 
 static_assert(static_vector {2, 3, 4} == slice<3>(static_vector {1, 2, 3, 4}, 1), "slice with offset");
 
-
 // Uniform initialization
 constexpr static_matrix<value, 3, 3> m1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
@@ -85,7 +85,8 @@ static_assert(m1.row(2) == static_vector {value(7), value(8), value(9)}, "static
 
 static_assert(m1.column(2) == static_vector {value(3), value(6), value(9)}, "static_matrix column");
 
-static_assert(fill<2, 2>(value(3)) == static_matrix {{{value(3), value(3)}, {value(3), value(3)}}}, "static_matrix fill");
+static_assert(fill<2, 2>(value(3)) == static_matrix {{{value(3), value(3)}, {value(3), value(3)}}},
+              "static_matrix fill");
 
 static_assert(matmul(m1, m1) == static_matrix<value, 3, 3> {{{30, 36, 42}, {66, 81, 96}, {102, 126, 150}}},
               "real static_matrix multiply");
@@ -99,7 +100,8 @@ static_assert(inverse(m22) == static_matrix<value, 2, 2> {{{7, -3}, {-2, 1}}}, "
 static_assert(matmul(inverse(m22), static_matrix<value, 2, 1> {{{1}, {1}}}) == static_matrix<value, 2, 1> {{{4}, {-1}}},
               "A^-1*b = x");
 
-static_assert(horzcat(identity<value, 2>, identity<value, 2>) == static_matrix<value, 2, 4> {{{1, 0, 1, 0}, {0, 1, 0, 1}}},
+static_assert(horzcat(identity<value, 2>, identity<value, 2>) ==
+                  static_matrix<value, 2, 4> {{{1, 0, 1, 0}, {0, 1, 0, 1}}},
               "horzcat");
 
 static_assert(submat<2, 2>(m1, 1, 1) == static_matrix<value, 2, 2> {{{5, 6}, {8, 9}}}, "submat");
@@ -107,4 +109,3 @@ static_assert(submat<2, 2>(m1, 1, 1) == static_matrix<value, 2, 2> {{{5, 6}, {8,
 static_assert(rref(m1) == static_matrix<value, 3, 3> {{{1, 0, -1}, {0, 1, 2}, {0, 0, 0}}}, "rref");
 
 static_assert(rank(m1) == 2, "rank");
-
