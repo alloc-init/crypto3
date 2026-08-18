19089034ce11af4b4ee201877d27efcc8f977cee
Brent Carmer <bcarmer@gmail.com>
2026-08-18 10:14:39 -0700

    support for other types

--- libs/marshalling/math/include/nil/crypto3/marshalling/math/types/matrix.hpp
+++ libs/marshalling/math/include/nil/crypto3/marshalling/math/types/matrix.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <cstddef>
+#include <limits>
 #include <stdexcept>
 #include <tuple>
 #include <utility>
@@ -17,8 +18,93 @@
 
 #include <nil/crypto3/marshalling/algebra/types/field_element.hpp>
 #include <nil/crypto3/math/matrix/compressed.hpp>
+#include <nil/crypto3/math/matrix/regular.hpp>
 
 namespace nil::crypto3::marshalling::types {
+
+    template<typename TTypeBase, typename MatrixType>
+    struct regular_matrix {
+        using index_type = nil::marshalling::types::integral<TTypeBase, std::size_t>;
+        using value_type = field_element<TTypeBase, typename MatrixType::value_type>;
+        using values_type = nil::marshalling::types::standard_array_list<TTypeBase, value_type>;
+        using type = nil::marshalling::types::bundle<
+            TTypeBase, std::tuple<index_type, index_type, values_type>>;
+    };
+
+    template<typename Endianness, typename MatrixType>
+    typename regular_matrix<nil::marshalling::field_type<Endianness>, MatrixType>::type
+        fill_regular_matrix(const MatrixType &matrix) {
+        using TTypeBase = nil::marshalling::field_type<Endianness>;
+        using marshalling_type = regular_matrix<TTypeBase, MatrixType>;
+
+        typename marshalling_type::values_type values;
+        values.value().reserve(matrix.rows() * matrix.columns());
+        for (std::size_t row = 0; row < matrix.rows(); ++row) {
+            for (std::size_t column = 0; column < matrix.columns(); ++column) {
+                values.value().emplace_back(matrix(row, column));
+            }
+        }
+
+        return typename marshalling_type::type(std::make_tuple(
+            typename marshalling_type::index_type(matrix.rows()),
+            typename marshalling_type::index_type(matrix.columns()),
+            std::move(values)));
+    }
+
+    template<typename Endianness, typename MatrixType>
+    MatrixType make_regular_matrix(
+        const typename regular_matrix<nil::marshalling::field_type<Endianness>, MatrixType>::type &filled_matrix) {
+        const auto &fields = filled_matrix.value();
+        const std::size_t rows = std::get<0>(fields).value();
+        const std::size_t columns = std::get<1>(fields).value();
+        const auto &values = std::get<2>(fields).value();
+        if (columns != 0 && rows > std::numeric_limits<std::size_t>::max() / columns) {
+            throw std::invalid_argument("regular matrix dimensions overflow");
+        }
+        if (values.size() != rows * columns) {
+            throw std::invalid_argument("regular matrix element count does not match its dimensions");
+        }
+
+        MatrixType matrix(rows, columns);
+        auto value = values.begin();
+        for (std::size_t row = 0; row < rows; ++row) {
+            for (std::size_t column = 0; column < columns; ++column, ++value) {
+                matrix(row, column) = value->value();
+            }
+        }
+        return matrix;
+    }
+
+    template<typename TTypeBase, typename VectorType>
+    struct regular_vector {
+        using value_type = field_element<TTypeBase, typename VectorType::value_type>;
+        using type = nil::marshalling::types::standard_array_list<TTypeBase, value_type>;
+    };
+
+    template<typename Endianness, typename VectorType>
+    typename regular_vector<nil::marshalling::field_type<Endianness>, VectorType>::type
+        fill_regular_vector(const VectorType &vector) {
+        using TTypeBase = nil::marshalling::field_type<Endianness>;
+        using marshalling_type = regular_vector<TTypeBase, VectorType>;
+
+        typename marshalling_type::type values;
+        values.value().reserve(vector.size());
+        for (std::size_t index = 0; index < vector.size(); ++index) {
+            values.value().emplace_back(vector[index]);
+        }
+        return values;
+    }
+
+    template<typename Endianness, typename VectorType>
+    VectorType make_regular_vector(
+        const typename regular_vector<nil::marshalling::field_type<Endianness>, VectorType>::type &filled_vector) {
+        const auto &values = filled_vector.value();
+        VectorType vector(values.size());
+        for (std::size_t index = 0; index < values.size(); ++index) {
+            vector[index] = values[index].value();
+        }
+        return vector;
+    }
 
     template<typename TTypeBase, typename MatrixType>
     struct compressed_matrix {
@@ -73,6 +159,54 @@ namespace nil::crypto3::marshalling::types {
             matrix(row, column) = std::get<2>(entry).value();
         }
         return matrix;
+    }
+
+    template<typename TTypeBase, typename VectorType>
+    struct compressed_vector {
+        using index_type = nil::marshalling::types::integral<TTypeBase, std::size_t>;
+        using value_type = field_element<TTypeBase, typename VectorType::value_type>;
+        using entry_type = nil::marshalling::types::bundle<TTypeBase, std::tuple<index_type, value_type>>;
+        using entries_type = nil::marshalling::types::standard_array_list<TTypeBase, entry_type>;
+        using type = nil::marshalling::types::bundle<TTypeBase, std::tuple<index_type, entries_type>>;
+    };
+
+    template<typename Endianness, typename VectorType>
+    typename compressed_vector<nil::marshalling::field_type<Endianness>, VectorType>::type
+        fill_compressed_vector(const VectorType &vector) {
+        using TTypeBase = nil::marshalling::field_type<Endianness>;
+        using marshalling_type = compressed_vector<TTypeBase, VectorType>;
+
+        typename marshalling_type::entries_type entries;
+        nil::crypto3::math::for_each_nonzero(
+            vector, [&](std::size_t index, const typename VectorType::value_type &value) {
+                entries.value().emplace_back(std::make_tuple(
+                    typename marshalling_type::index_type(index),
+                    typename marshalling_type::value_type(value)));
+            });
+
+        return typename marshalling_type::type(std::make_tuple(
+            typename marshalling_type::index_type(vector.size()), std::move(entries)));
+    }
+
+    template<typename Endianness, typename VectorType>
+    VectorType make_compressed_vector(
+        const typename compressed_vector<nil::marshalling::field_type<Endianness>, VectorType>::type &filled_vector) {
+        const auto &fields = filled_vector.value();
+        const std::size_t size = std::get<0>(fields).value();
+        VectorType vector(size);
+
+        for (const auto &filled_entry : std::get<1>(fields).value()) {
+            const auto &entry = filled_entry.value();
+            const std::size_t index = std::get<0>(entry).value();
+            if (index >= size) {
+                throw std::invalid_argument("compressed vector entry is out of bounds");
+            }
+            if (nil::crypto3::math::find_element(vector, index) != nullptr) {
+                throw std::invalid_argument("compressed vector contains a duplicate entry");
+            }
+            vector[index] = std::get<1>(entry).value();
+        }
+        return vector;
     }
 
 }    // namespace nil::crypto3::marshalling::types
--- libs/marshalling/math/test/matrix.cpp
+++ libs/marshalling/math/test/matrix.cpp
@@ -8,6 +8,7 @@
 #include <nil/crypto3/algebra/fields/fp12_2over3over2.hpp>
 #include <nil/crypto3/marshalling/math/types/matrix.hpp>
 #include <nil/crypto3/math/matrix/compressed.hpp>
+#include <nil/crypto3/math/matrix/regular.hpp>
 #include <nil/marshalling/endianness.hpp>
 #include <nil/marshalling/status_type.hpp>
 
@@ -17,27 +18,33 @@ namespace {
     namespace types = nil::crypto3::marshalling::types;
 
     using fp12_type = fields::fp12_2over3over2<fields::alt_bn128<254>>::value_type;
-    using matrix_type = math::compressed_matrix<fp12_type>;
+    using compressed_matrix_type = math::compressed_matrix<fp12_type>;
+    using compressed_vector_type = math::compressed_vector<fp12_type>;
+    using regular_matrix_type = math::regular_matrix<fp12_type>;
+    using regular_vector_type = math::regular_vector<fp12_type>;
 
-    template<typename Endianness>
-    void check_round_trip() {
-        matrix_type input(3, 5);
-        input(0, 4) = fp12_type::one();
-        input(2, 1) = fp12_type::one() + fp12_type::one();
-
-        auto filled = types::fill_compressed_matrix<Endianness>(input);
+    template<typename FilledType>
+    FilledType encode_and_decode(FilledType filled) {
         std::vector<std::uint8_t> bytes(filled.length());
         auto write_iter = bytes.begin();
         BOOST_REQUIRE(filled.write(write_iter, bytes.size()) == nil::marshalling::status_type::success);
 
-        using filled_type = typename types::compressed_matrix<
-            nil::marshalling::field_type<Endianness>, matrix_type>::type;
-        filled_type decoded;
+        FilledType decoded;
         auto read_iter = bytes.begin();
         BOOST_REQUIRE(decoded.read(read_iter, bytes.size()) == nil::marshalling::status_type::success);
         BOOST_REQUIRE(read_iter == bytes.end());
+        return decoded;
+    }
 
-        const auto output = types::make_compressed_matrix<Endianness, matrix_type>(decoded);
+    template<typename Endianness>
+    void check_compressed_matrix_round_trip() {
+        compressed_matrix_type input(3, 5);
+        input(0, 4) = fp12_type::one();
+        input(2, 1) = fp12_type::one() + fp12_type::one();
+
+        auto filled = types::fill_compressed_matrix<Endianness>(input);
+        const auto decoded = encode_and_decode(std::move(filled));
+        const auto output = types::make_compressed_matrix<Endianness, compressed_matrix_type>(decoded);
         BOOST_CHECK_EQUAL(output.rows(), 3);
         BOOST_CHECK_EQUAL(output.columns(), 5);
         BOOST_REQUIRE(math::find_element(output, 0, 4) != nullptr);
@@ -46,12 +53,77 @@ namespace {
         BOOST_CHECK(*math::find_element(output, 2, 1) == fp12_type::one() + fp12_type::one());
         BOOST_CHECK(math::find_element(output, 1, 1) == nullptr);
     }
+
+    template<typename Endianness>
+    void check_regular_matrix_round_trip() {
+        regular_matrix_type input(2, 3);
+        fp12_type value = fp12_type::one();
+        for (std::size_t row = 0; row < input.rows(); ++row) {
+            for (std::size_t column = 0; column < input.columns(); ++column) {
+                input(row, column) = value;
+                value += fp12_type::one();
+            }
+        }
+
+        const auto decoded = encode_and_decode(types::fill_regular_matrix<Endianness>(input));
+        const auto output = types::make_regular_matrix<Endianness, regular_matrix_type>(decoded);
+        BOOST_CHECK_EQUAL(output.rows(), input.rows());
+        BOOST_CHECK_EQUAL(output.columns(), input.columns());
+        for (std::size_t row = 0; row < input.rows(); ++row) {
+            for (std::size_t column = 0; column < input.columns(); ++column) {
+                BOOST_CHECK(output(row, column) == input(row, column));
+            }
+        }
+    }
+
+    template<typename Endianness>
+    void check_regular_vector_round_trip() {
+        regular_vector_type input(3);
+        input[0] = fp12_type::one();
+        input[1] = fp12_type::one() + fp12_type::one();
+        input[2] = fp12_type::one() + fp12_type::one() + fp12_type::one();
+
+        const auto decoded = encode_and_decode(types::fill_regular_vector<Endianness>(input));
+        const auto output = types::make_regular_vector<Endianness, regular_vector_type>(decoded);
+        BOOST_REQUIRE_EQUAL(output.size(), input.size());
+        for (std::size_t index = 0; index < input.size(); ++index) {
+            BOOST_CHECK(output[index] == input[index]);
+        }
+    }
+
+    template<typename Endianness>
+    void check_compressed_vector_round_trip() {
+        compressed_vector_type input(5);
+        input[1] = fp12_type::one();
+        input[4] = fp12_type::one() + fp12_type::one();
+
+        const auto decoded = encode_and_decode(types::fill_compressed_vector<Endianness>(input));
+        const auto output = types::make_compressed_vector<Endianness, compressed_vector_type>(decoded);
+        BOOST_CHECK_EQUAL(output.size(), input.size());
+        BOOST_REQUIRE(math::find_element(output, 1) != nullptr);
+        BOOST_REQUIRE(math::find_element(output, 4) != nullptr);
+        BOOST_CHECK(*math::find_element(output, 1) == input[1]);
+        BOOST_CHECK(*math::find_element(output, 4) == input[4]);
+        BOOST_CHECK(math::find_element(output, 2) == nullptr);
+    }
 }    // namespace
 
 BOOST_AUTO_TEST_CASE(compressed_fp12_matrix_big_endian) {
-    check_round_trip<nil::marshalling::option::big_endian>();
+    check_compressed_matrix_round_trip<nil::marshalling::option::big_endian>();
 }
 
 BOOST_AUTO_TEST_CASE(compressed_fp12_matrix_little_endian) {
-    check_round_trip<nil::marshalling::option::little_endian>();
+    check_compressed_matrix_round_trip<nil::marshalling::option::little_endian>();
+}
+
+BOOST_AUTO_TEST_CASE(other_fp12_matrix_types_big_endian) {
+    check_regular_matrix_round_trip<nil::marshalling::option::big_endian>();
+    check_regular_vector_round_trip<nil::marshalling::option::big_endian>();
+    check_compressed_vector_round_trip<nil::marshalling::option::big_endian>();
+}
+
+BOOST_AUTO_TEST_CASE(other_fp12_matrix_types_little_endian) {
+    check_regular_matrix_round_trip<nil::marshalling::option::little_endian>();
+    check_regular_vector_round_trip<nil::marshalling::option::little_endian>();
+    check_compressed_vector_round_trip<nil::marshalling::option::little_endian>();
 }
